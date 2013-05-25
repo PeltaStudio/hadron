0bf5584dfa5d17d465f4af8b40689e8e4bb51b5e
lodr <salva@unoyunodiez.com>
2013-05-25 18:38:08 +0200

    Numbering hexagons

--- js/logo/GameOfLife.js
+++ js/logo/GameOfLife.js
@@ -4,76 +4,99 @@ define(function (require) {
   var T = require('hadron/toolkit'),
       Model = require('hadron/Model'),
       Hexagon = require('Hexagon'),
-      HexCell = require('HexCell');
+      HexCell = require('HexCell'),
+      FOSSIL_CELL;
 
-  function GameOfLife(side, size, center) {
+  // Ad-hoc completely dead (no render / no behaviour) cell
+  FOSSIL_CELL = new HexCell(0, [0,0], false);
+  FOSSIL_CELL.render = void 0;
+  FOSSIL_CELL.simulate = void 0;
+
+  function GameOfLife(side, cellRadius, center) {
     var self = this;
     Model.apply(this, arguments);
 
-    var BOARD_SIZE = 150,
-        FILL_AREA = Math.sqrt(3) * size * (side - 1);
+    var BOARD_cellRadius = 150,
+        START_OF_THE_FILL_AREA = Math.sqrt(3) * cellRadius * (side - 1),
+        board = [], cols, rows;
 
+    setupFrame();
     setupBoard();
-    setupTop();
-    setupMiddle();
-    setupBottom();
 
-    function setupBoard() {
-      var board = new Hexagon(BOARD_SIZE, center);
+    function setupFrame() {
+      var board = new Hexagon(BOARD_cellRadius, center);
       board.fillColor = 'white';
       board.rotation = Math.PI / 2;
       self.add(board);
     }
 
-    // 0-based row counting
-    function setupTop() {
-      var cell, row, col, colCount, X, Y, hexagonsByRow;
-      for (row = 0; row < side; row++) {
-        X = center[0] - (3/2 * size * row);
-        Y = center[1] - FILL_AREA + (size * Math.sqrt(3)/2 * row);
-        hexagonsByRow = row + 1;
-        for (col = 0, colCount = hexagonsByRow; col < colCount; col++) {
-          cell = new HexCell(size, [X, Y]);
+    function setupBoard() {
+      var cell,
+          isRowOdd, evenRowStartingX, oddRowStartingX,
+          x, y, stepX, stepY;
+
+      cols = side;
+      rows = ((side * 2 - 1) * 2) - 1;
+
+      stepX = 3 * cellRadius;
+      stepY = Math.sqrt(3)/2 * cellRadius;
+      oddRowStartingX = center[0] - (3/2 * cellRadius * (side - 1));
+      evenRowStartingX = center[0] - (3/2 * cellRadius * (side - 2));
+
+      y = center[1] - START_OF_THE_FILL_AREA;
+      for (var r = 0; r < rows; r++) {
+        board[r] = [];
+        isRowOdd = (r % 2 === 1);
+        x = isRowOdd ? oddRowStartingX : evenRowStartingX;
+        for (var c = 0; c < cols; c++) {
+          cell = isOutOfTheHexagon(r, c) ? FOSSIL_CELL :
+                                           new HexCell(cellRadius, [x, y]);
+          board[r][c] = cell;
           self.add(cell);
-          X += 3 * size;
+          x += stepX;
         }
+        y += stepY;
       }
     }
 
-    function setupMiddle() {
-      var cell, row, lastRow, col, colCount, X, Y, hexagonsByRow,
-          fillingHoles = true,
-          fillingStartingPosition = center[0] - (3/2 * size * (side - 2)),
-          defaultStartingPosition = center[0] - (3/2 * size * (side - 1)),
-          holes = side - 1;
+    // Determine if a pair row, colum is inside the a hexagon shape according
+    // to the size of the hexagon (`side`). A pair is inside the hexagon if
+    // it is in the central zone, or inside one of the triangles that are over
+    // and below the central zone.
+    function isOutOfTheHexagon(r, c) {
+      var topBoundary, bottomBoundary,
+          leftBoundary, rightBoundary,
+          isRowEven = (r % 2 === 0),
+          middle = (side / 2) - 1,
+          index; // index in the odd/even serie
 
-      lastRow = side + 2 * (side - 1);
-      for (row = side; row < lastRow; row++) {
-        X = fillingHoles ? fillingStartingPosition : defaultStartingPosition;
-        Y = center[1] - FILL_AREA + (size * Math.sqrt(3)/2 * row);
-        hexagonsByRow = fillingHoles ? holes : side;
-        for (col = 0, colCount = hexagonsByRow; col < colCount; col++) {
-          cell = new HexCell(size, [X, Y]);
-          self.add(cell);
-          X += 3 * size;
-        }
-        fillingHoles = !fillingHoles;
-
+      // TODO: this is the last column; could it be generalized?
+      if (isRowEven && c === side - 1 || r === side * 4) {
+        return true;
       }
-    }
 
-    function setupBottom() {
-      var cell, rowOffset, i, row, col, colCount, X, Y, hexagonsByRow;
-      rowOffset = side + 2 * (side - 1);
-      for (row = side - 2, i = 0; row >= 0; row--, i++) {
-        X = center[0] - (3/2 * size * row);
-        Y = center[1] - FILL_AREA + (size * Math.sqrt(3)/2 * (rowOffset + i));
-        hexagonsByRow = row + 1;
-        for (col = 0, colCount = hexagonsByRow; col < colCount; col++) {
-          cell = new HexCell(size, [X, Y]);
-          self.add(cell);
-          X += 3 * size;
+      topBoundary = side - 1;
+      bottomBoundary = rows - side;
+
+      if (topBoundary <= r && r <= bottomBoundary) {
+        return false;
+      }
+      else {
+        if (r > bottomBoundary) {
+          r -= bottomBoundary;
+          r = side - r;
         }
+
+        index = isRowEven ? r / 2 : (r - 1) / 2;
+        if (isRowEven) {
+          leftBoundary = middle - index;
+          rightBoundary = middle + index;
+        }
+        else {
+          leftBoundary = middle - index;
+          rightBoundary = (middle + index) + 1;
+        }
+        return c < leftBoundary || c > rightBoundary;
       }
     }
   }
